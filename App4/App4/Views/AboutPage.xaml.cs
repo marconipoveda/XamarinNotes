649d327c7c69ece4d6883185bdc80d5011fd5a40
Marconi Poveda <marconipoveda84@gmail.com>
2021-02-26 12:21:59 -0600

    Add project files.

---
+++ App4/App4/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("https://aka.ms/xamarin-quickstart");
+        }
+    }
+}
---
+++ App4/App4/Views/NotesPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace App4.Views
+{
+    /*[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+    }*/
+
+    public partial class NotesPage: ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"notes.txt");
+
+        public NotesPage()
+        {
+            InitializeComponent();
+
+            if(File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e) 
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
